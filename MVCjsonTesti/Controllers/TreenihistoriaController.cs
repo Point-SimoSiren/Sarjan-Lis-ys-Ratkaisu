3548ce3d9d639b2dc86d458808f0b29098eb7a8d
Pont-SimoSiren <sirensimo7@gmail.com>
2024-11-14 14:04:00 +0200

    Add project files.

---
+++ MVCjsonTesti/Controllers/TreenihistoriaController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCjsonTesti.Models;
+
+namespace MVCjsonTesti.Controllers
+{
+    public class TreenihistoriaController : Controller
+    {
+        private TreenausDBEntities db = new TreenausDBEntities();
+
+
+
+        //----------------LUODAAN LOMAKE SARJAN LISÄÄMISTÄ VARTEN---------------------------------------------------------------------
+        // ------ Treenihistorian id tulee parametrina TREENIHISTORIA / INDEX.CSHTML SIVULTA
+
+        public ActionResult Sarja(int? id)
+        {
+            ViewBag.TreeniId = id;
+            return View();
+        }
+
+      
+
+        // ----------- SARJAN LISÄYS HTTP POST -------------------------------
+
+     
+        [HttpPost]
+        public ActionResult Sarja(Treenihistoriarivit treenihistoriarivit)
+        {
+          
+                db.Treenihistoriarivit.Add(treenihistoriarivit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+        }
+
+
+        //----------------------------------------------------------------------------------------------------
+
+
+
+        // GET: Treenihistoria
+        public ActionResult Index()
+        {
+            return View(db.Treenihistoria.ToList());
+        }
+
+        // GET: Treenihistoria/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treenihistoria treenihistoria = db.Treenihistoria.Find(id);
+            if (treenihistoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treenihistoria);
+        }
+
+        // GET: Treenihistoria/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: Treenihistoria/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TreenihistoriaID,Pvm")] Treenihistoria treenihistoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Treenihistoria.Add(treenihistoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(treenihistoria);
+        }
+
+        // GET: Treenihistoria/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treenihistoria treenihistoria = db.Treenihistoria.Find(id);
+            if (treenihistoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treenihistoria);
+        }
+
+        // POST: Treenihistoria/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TreenihistoriaID,Pvm")] Treenihistoria treenihistoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(treenihistoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(treenihistoria);
+        }
+
+        // GET: Treenihistoria/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treenihistoria treenihistoria = db.Treenihistoria.Find(id);
+            if (treenihistoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treenihistoria);
+        }
+
+        // POST: Treenihistoria/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Treenihistoria treenihistoria = db.Treenihistoria.Find(id);
+            db.Treenihistoria.Remove(treenihistoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCjsonTesti/Controllers/TreenihistoriarivitController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCjsonTesti.Models;
+
+namespace MVCjsonTesti.Controllers
+{
+    public class TreenihistoriarivitController : Controller
+    {
+        private TreenausDBEntities db = new TreenausDBEntities();
+
+        // GET: Treenihistoriarivit
+        public ActionResult Index()
+        {
+            var treenihistoriarivit = db.Treenihistoriarivit.Include(t => t.Treenihistoria);
+            return View(treenihistoriarivit.ToList());
+        }
+
+        // GET: Treenihistoriarivit/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treenihistoriarivit treenihistoriarivit = db.Treenihistoriarivit.Find(id);
+            if (treenihistoriarivit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treenihistoriarivit);
+        }
+
+  
+
+        // SARJOJEN LISÄYS TAPAHTUU TOISESSA KONTROLLERISSA!!
+
+        // SIKSI CREATE GET JA POST METODIT TREENIHISTORIARIVEILLE PUUTTUVAT TÄÄLTÄ
+
+
+
+
+        // GET: Treenihistoriarivit/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treenihistoriarivit treenihistoriarivit = db.Treenihistoriarivit.Find(id);
+            if (treenihistoriarivit == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TreenihistoriaID = new SelectList(db.Treenihistoria, "TreenihistoriaID", "TreenihistoriaID", treenihistoriarivit.TreenihistoriaID);
+            return View(treenihistoriarivit);
+        }
+
+        // POST: Treenihistoriarivit/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TreenihistoriariviID,TreenihistoriaID,LiikeID,Sarjat,Painot")] Treenihistoriarivit treenihistoriarivit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(treenihistoriarivit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TreenihistoriaID = new SelectList(db.Treenihistoria, "TreenihistoriaID", "TreenihistoriaID", treenihistoriarivit.TreenihistoriaID);
+            return View(treenihistoriarivit);
+        }
+
+        // GET: Treenihistoriarivit/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Treenihistoriarivit treenihistoriarivit = db.Treenihistoriarivit.Find(id);
+            if (treenihistoriarivit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(treenihistoriarivit);
+        }
+
+        // POST: Treenihistoriarivit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Treenihistoriarivit treenihistoriarivit = db.Treenihistoriarivit.Find(id);
+            db.Treenihistoriarivit.Remove(treenihistoriarivit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
